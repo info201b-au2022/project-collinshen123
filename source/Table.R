6827d993da2b7346d4ff5de6dbd09f1c4433b838
JaimeBerd <105523676+JaimeBerd@users.noreply.github.com>
2022-11-14 12:18:01 -0800

    Update Table.R

---
+++ source/Table.R
@@ -0,0 +1,48 @@
+# Table for Part 2
+
+# Joining data for each state from exercise, race, and overall
+library(readxl)
+library(dplyr)
+data_table <- read_excel("C://Users/fatni/documents/info201/project-collinshen123/data/Physical_Activity_Table.xlsx")
+data_table <- data_table %>% 
+  filter(is.na(County)) %>% 
+  select(State, `Male sufficient physical activity  prevalence, 2011* (%)`, `Female sufficient physical activity  prevalence, 2011* (%)`)
+
+white_table <- read.csv("C://Users/fatni/documents/info201/project-collinshen123/data/White_Obesity_Rates.csv")
+asian_table <- read.csv("C://Users/fatni/documents/info201/project-collinshen123/data/Asian_Obesity_Rates.csv")
+AIAN_table <- read.csv("C://Users/fatni/documents/info201/project-collinshen123/data/AIAN_Obesity_Rates.csv")
+black_table <- read.csv("C://Users/fatni/documents/info201/project-collinshen123/data/Black_Obesity_Rates.csv")
+hispanic_table <- read.csv("C://Users/fatni/documents/info201/project-collinshen123/data/Hispanic_Obesity_Rates.csv")
+
+filter_columns <- function(table) {
+  table <- table %>% 
+    select(State, Prevalence)
+  
+  table[table == "Insufficient data*"] <- NA
+  
+  return(table)
+}
+
+white_table <- filter_columns(white_table)
+colnames(white_table)[colnames(white_table) == "Prevalence"] <- "White Obesity Rate"
+
+asian_table <- filter_columns(asian_table)
+colnames(asian_table)[colnames(asian_table) == "Prevalence"] <- "Asian Obesity Rate"
+
+AIAN_table <- filter_columns(AIAN_table)
+colnames(AIAN_table)[colnames(AIAN_table) == "Prevalence"] <- "American Indian/Alaskan Native Obesity Rate"
+
+black_table <- filter_columns(black_table)
+colnames(black_table)[colnames(black_table) == "Prevalence"] <- "Black Obesity Rate"
+
+hispanic_table <- filter_columns(hispanic_table)
+colnames(hispanic_table)[colnames(hispanic_table) == "Prevalence"] <- "Hispanic Obesity Rate"
+
+race_table <- left_join(white_table, asian_table, by = "State") %>% 
+  left_join(AIAN_table, by = "State") %>% 
+  left_join(black_table, by = "State") %>% 
+  left_join(hispanic_table, by = "State")
+
+race_table <- filter(race_table, )
+
+View(race_table)
