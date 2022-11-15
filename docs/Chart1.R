b42499a6f0333bd1b4bd9cb9c30f6718cdd3d62e
Collin Shen <collinshen123@gmail.com>
2022-11-14 16:14:38 -0800

    Hispanic filtered data

--- docs/Chart1.R
+++ docs/Chart1.R
@@ -4,7 +4,7 @@ library(stringr)
 
 AsianRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/Asian_Obesity_Rates.csv", stringsAsFactors = FALSE )
 WhiteRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/White_Obesity_Rates.csv", stringsAsFactors = FALSE )
-BlackRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/Black_Obesity_Rates.csv", stringsAsFactors = FALSE )
+BlackRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/Black_Obesity_Rates.csv", stringsAsFactors = FALSE )
 HispanicRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/Hispanic_Obesity_Rates.csv", stringsAsFactors = FALSE )
 AIANRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/AIAN_Obesity_Rates.csv", stringsAsFactors = FALSE )
 
@@ -18,9 +18,17 @@ White_in_Wa <- WhiteRates%>%
 Asian_in_Wa <- AsianRates%>%
   mutate(Race = "Asian in WA")%>%
   filter(str_detect(State, "Washington"))
-  
+
 updated_table <- White_in_Wa %>%
   rbind(Asian_in_Wa)
+
+Black_in_Wa <- BlackRates%>%
+  mutate(Race = "Black in WA")%>%
+  filter(str_detect(State, "Washington"))
+  
+updated_table2 <- updated_table %>%
+  rbind(Black_in_Wa)
+
 
 
 install.packages("tidyverse")
