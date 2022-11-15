33ecb1b2d39b3183d0a4ec5f00a556497c4308c2
Collin Shen <collinshen123@gmail.com>
2022-11-14 16:00:41 -0800

    Race chart update

--- docs/Chart1.R
+++ docs/Chart1.R
@@ -1,15 +1,26 @@
 
 library(dplyr)
+library(stringr)
 
-AsianRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/Asian_Obesity_Rates.csv", stringsAsFactors = FALSE )
-WhiteRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/White_Obesity_Rates.csv", stringsAsFactors = FALSE )
+AsianRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/Asian_Obesity_Rates.csv", stringsAsFactors = FALSE )
+WhiteRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/White_Obesity_Rates.csv", stringsAsFactors = FALSE )
 BlackRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/Black_Obesity_Rates.csv", stringsAsFactors = FALSE )
 HispanicRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/Hispanic_Obesity_Rates.csv", stringsAsFactors = FALSE )
 AIANRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/AIAN_Obesity_Rates.csv", stringsAsFactors = FALSE )
 
 
-a <- AsianRates %>%
-  full_join(WhiteRates, by = "State")
+
+White_in_Wa <- WhiteRates%>%
+  mutate(Race = "White in WA")%>%
+  filter(str_detect(State, "Washington"))
+  
+ 
+Asian_in_Wa <- AsianRates%>%
+  mutate(Race = "Asian in WA")%>%
+  filter(str_detect(State, "Washington"))
+  
+updated_table <- White_in_Wa %>%
+  rbind(Asian_in_Wa)
 
 
 install.packages("tidyverse")
@@ -17,6 +28,6 @@ install.packages("ggplot2")
 library(ggplot2)
 
 
-ggplot(data = a) + 
-  geom_col(mapping = aes(x = State, y = Prevalence))
+ggplot(data = updated_table) + 
+  geom_col(mapping = aes(x = Prevalence, y = Race))
 
