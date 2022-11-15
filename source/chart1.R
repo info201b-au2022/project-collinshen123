16c76d384480f6787c7cf67fb5d363963d1da041
JaimeBerd <105523676+JaimeBerd@users.noreply.github.com>
2022-11-14 21:07:21 -0800

    update index.rmd

--- source/chart1.R
+++ source/chart1.R
@@ -6,27 +6,6 @@ female_physical_activity <- read.csv("https://raw.githubusercontent.com/info201b
 female_physical_activity <- female_physical_activity %>% 
   filter(County == "") %>% 
   select(State, Female.sufficient.physical.activity..prevalence..2011.....)
-<<<<<<< HEAD
-
-colnames(female_physical_activity)[colnames(female_physical_activity) == "Female.sufficient.physical.activity..prevalence..2011....."] <- "Female Sufficient Physical Activity Prevalence (%)"
-
-female_obesity <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/County_Obesity_Table.csv")
-female_obesity <- female_obesity %>% 
-  filter(County == "") %>% 
-  select(State, Female.obesity.prevalence..2011.....) 
-colnames(female_obesity)[colnames(female_obesity) == "Female.obesity.prevalence..2011....."] <- "Female Obesity Prevalence (%)"
-
-female_physical_activity <- left_join(female_physical_activity, female_obesity, by = "State") %>% 
-  drop_na() %>% 
-  select (-State)
-
-View(female_physical_activity)
-
-library(ggplot2)
-# Basic scatter plot
-ggplot(female_physical_activity, aes(x=`Female Sufficient Physical Activity Prevalence (%)`, y=`Female Obesity Prevalence (%)`)) + geom_point()
-=======
->>>>>>> b420f51af7e45630a4145b01705f28ec06675e11
 
 colnames(female_physical_activity)[colnames(female_physical_activity) == "Female.sufficient.physical.activity..prevalence..2011....."] <- "Female Sufficient Physical Activity Prevalence (%)"
 
