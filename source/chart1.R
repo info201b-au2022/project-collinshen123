6d12b363b0a3af7fe6cf6e636570281fec1c74c6
JaimeBerd <105523676+JaimeBerd@users.noreply.github.com>
2022-11-14 23:48:20 -0800

    reverse chart1

--- source/chart1.R
+++ source/chart1.R
@@ -1,46 +1,42 @@
-
+# Chart 2: Exercise vs Obesity % by sex
 library(dplyr)
-library(stringr)
+library(tidyr)
+female_physical_activity <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/Physical_Activity_Table.csv")
+female_physical_activity <- female_physical_activity %>% 
+  filter(County == "") %>% 
+  select(State, Female.sufficient.physical.activity..prevalence..2011.....)
 
-AsianRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/Asian_Obesity_Rates.csv", stringsAsFactors = FALSE )
-WhiteRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/White_Obesity_Rates.csv", stringsAsFactors = FALSE )
-BlackRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/Black_Obesity_Rates.csv", stringsAsFactors = FALSE )
-HispanicRates <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/Hispanic_Obesity_Rates%20.csv", stringsAsFactors = FALSE )
-AIANRates <- read.csv("/Users/frank/Documents/Collin/code/info201/project-collinshen123/data/AIAN_Obesity_Rates.csv", stringsAsFactors = FALSE )
+colnames(female_physical_activity)[colnames(female_physical_activity) == "Female.sufficient.physical.activity..prevalence..2011....."] <- "Female Sufficient Physical Activity Prevalence (%)"
 
+female_obesity <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/County_Obesity_Table.csv")
+female_obesity <- female_obesity %>% 
+  filter(County == "") %>% 
+  select(State, Female.obesity.prevalence..2011.....) 
+colnames(female_obesity)[colnames(female_obesity) == "Female.obesity.prevalence..2011....."] <- "Female Obesity Prevalence (%)"
 
+female_physical_activity <- left_join(female_physical_activity, female_obesity, by = "State") %>% 
+  drop_na() %>% 
+  select (-State)
 
-White_in_Wa <- WhiteRates%>%
-  mutate(Race = "White in WA")%>%
-  filter(str_detect(State, "Washington"))
-  
- 
-Asian_in_Wa <- AsianRates%>%
-  mutate(Race = "Asian in WA")%>%
-  filter(str_detect(State, "Washington"))
+View(female_physical_activity)
 
-updated_table <- White_in_Wa %>%
-  rbind(Asian_in_Wa)
-
-Black_in_Wa <- BlackRates%>%
-  mutate(Race = "Black in WA")%>%
-  filter(str_detect(State, "Washington"))
-  
-updated_table2 <- updated_table %>%
-  rbind(Black_in_Wa)
-
-Hispanic_in_Wa <- HispanicRates%>%
-  mutate(Race = "Hispanic in WA")%>%
-  filter(str_detect(State, "Washington"))
-
-updated_table3 <- updated_table2 %>%
-  rbind(Hispanic_in_Wa)
-
-install.packages("tidyverse")
-install.packages("ggplot2")
 library(ggplot2)
+# Basic scatter plot
+ggplot(female_physical_activity, aes(x=`Female Sufficient Physical Activity Prevalence (%)`, y=`Female Obesity Prevalence (%)`)) + geom_point()
 
+colnames(female_physical_activity)[colnames(female_physical_activity) == "Female.sufficient.physical.activity..prevalence..2011....."] <- "Female Sufficient Physical Activity Prevalence (%)"
 
-ggplot(data = updated_table) + 
-  geom_col(mapping = aes(x = Prevalence, y = Race))
+female_obesity <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-collinshen123/main/data/County_Obesity_Table.csv")
+female_obesity <- female_obesity %>% 
+  filter(County == "") %>% 
+  select(State, Female.obesity.prevalence..2011.....) 
+colnames(female_obesity)[colnames(female_obesity) == "Female.obesity.prevalence..2011....."] <- "Female Obesity Prevalence (%)"
+female_physical_activity <- left_join(female_physical_activity, female_obesity, by = "State") %>% 
+  drop_na() %>% 
+  select (-State)
 
+View(female_physical_activity)
+
+library(ggplot2)
+# Basic scatter plot
+ggplot(female_physical_activity, aes(x=`Female Sufficient Physical Activity Prevalence (%)`, y=`Female Obesity Prevalence (%)`)) + geom_point()
