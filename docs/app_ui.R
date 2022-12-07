5d49799472a1eb274f463a6847183934b3d92ce1
Collin Shen <collinshen123@gmail.com>
2022-12-07 12:07:35 -0800

    added plot captions

--- docs/app_ui.R
+++ docs/app_ui.R
@@ -23,7 +23,17 @@ Second_Page <- tabPanel(
     mainPanel(
       plotOutput("exercise_output")
     )
-  )
+  ),
+  h4("Summary paragraph"),
+  p("This scatter plot shows the percentage of obesity prevalence 
+    compared to the percentage of sufficient physical activity prevalence 
+    in each state. The user can select which year (2001, 2009, 2011) and 
+    sex (male or female) and a scatterplot with these varibales will be displayed. 
+    This information is useful as it helps to gain a better understanding from 
+    data of the relationship between the prevalence of obesity when it is being 
+    compared to the prevalence of physical activity. While correlation does not 
+    necessarily mean causation, seeing these two variables together leaves the
+    plot up for interpretation by the user.")
 )
 
 Third_Page <-tabPanel(
@@ -39,7 +49,15 @@ Third_Page <-tabPanel(
   ),
   mainPanel(
     plotlyOutput("bar")
-  )
+  ),
+  h4("Summary paragraph"),
+  p("This bar graph displays the recorded percent prevalence of 
+    obesity by US state. Data for this graph was sourced from the 
+    Centers for DiseaseControl and Prevention (CDC) and was collected 
+    from 2019–2021. It is included as it presents valuable information 
+    of how prevalent obesity may be in a particular state. The user is 
+    able to select whatever state(s) of their choosing and visualize
+    how states may compare to one another when it comes to the prevalence of obesity. ")
   
  
 )
@@ -67,7 +85,17 @@ Fourth_Page <- tabPanel(
   
   map_main_content <- mainPanel(
     plotlyOutput("map")
-  )
+  ),
+  h4("Summary paragraph"),
+  p("Here we have a geographic map of the US representing the 
+    difference in obesity prevalence by race in the United States. 
+    The data was collected from CDC’s National Center for Health 
+    Statistics. This chart helped us reveal interesting insights 
+    for our future research, on whether race has any direct or 
+    indirect influence on obesity prevalence. The user is able to 
+    select a race and from there the visualization will display the
+    estimated number of people from a race group that are obese and
+    in which area of the US. ")
   
 )
 
